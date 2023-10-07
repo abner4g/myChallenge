10848a9f6b7c9142bf619cb17880ad1a0b00f7b9
abner4g <abner.marques89@gmail.com>
2023-10-07 14:34:20 -0300

    adjusted name class application

--- src/main/java/com/challengepicpay/ChallengePicpayApplication.java
+++ src/main/java/com/challengepicpay/ChallengePicpayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengepicpayApplication {
+public class ChallengePicpayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChallengepicpayApplication.class, args);
+		SpringApplication.run(ChallengePicpayApplication.class, args);
 	}
 
 }
